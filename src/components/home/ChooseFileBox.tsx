0fe7b8a71a189a9c955b65d488da88ac0a323688
Nguyễn Minh Lộc <loc.nguyenminh1605@hcmut.edu.vn>
2023-10-02 19:07:53 +0700

    fix(choosefilebox): fix some lg desktop but I don't fix width Dialogbody of choose file component

--- src/components/home/ChooseFileBox.tsx
+++ src/components/home/ChooseFileBox.tsx
@@ -17,43 +17,44 @@ export function useChooseFileBox() {
 
     return (
       <>
-        <Dialog open={openBox} handler={handleOpenBox}>
-          <DialogBody
-            onClick={() => {
-              setOpenBox(false);
-              openOrderWorkflowBox();
-              setOrderStep(1);
-            }}
-          >
-            <div>
+        <div className='relative'>
+          <Dialog open={openBox} handler={handleOpenBox}>
+            <DialogBody
+              className='flex flex-col items-center justify-center gap-5 p-5 lg:p-10'
+              onClick={() => {
+                setOpenBox(false);
+                openOrderWorkflowBox();
+                setOrderStep(1);
+              }}
+            >
               <label
                 htmlFor='dropzone-file'
-                className='flex flex-col items-center justify-center w-62 h-35 gap-4 p-4 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
+                className='flex flex-col w-70 h-35 gap-3 p-4 lg:w-96 lg:h-47 lg:p-8 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
               >
-                <div className='flex items-center justify-center gap-4 h-10'>
+                <div className='flex gap-4'>
                   <ArrowUpTrayIcon
                     strokeWidth={2}
-                    className='bg-blue/1 text-white rounded-full w-10 h-10 p-2 lg:w-10 lg:h-10 lg:p-3'
+                    className='bg-blue/1 text-white rounded-full w-10 h-10 p-2 lg:w-12 lg:h-12 lg:p-3'
                   />
-                  <span className='text-xl text-blue/1 font-semibold w-37 h-8'>
+                  <span className='justify-center text-xl lg:text-2xl text-blue/1 font-semibold h=10 p-1 lg:h-12 lg:p-2'>
                     Choose file to print
                   </span>
                 </div>
 
-                <div className='lg: text-xs w-54 h-13 gap-1'>
+                <div className='text-sm lg:text-md w-54 h-13 gap-1'>
                   <span className='font-semibold h-8'>Allowed formats:</span> .doc, .docx, .xls,
                   .xlsx, .ppt, .jpg, .png, .pdf
-                  <div className='lg: text-xs h-4'>
+                  <div className='text-sm lg:text-md h-4'>
                     <span className='font-semibold'>Maximum size:</span> 100MB
                   </div>
                 </div>
 
                 <input id='dropzone-file' type='file' className='hidden' />
               </label>
-            </div>
-          </DialogBody>
-        </Dialog>
-        {<OrderWorkflowBox />}
+            </DialogBody>
+          </Dialog>
+          {<OrderWorkflowBox />}
+        </div>
       </>
     );
   };
