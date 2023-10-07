8b1c4d4231d00cb391671b3be24c3538099c319d
ngyngcphu <nguyenngocphu225@gmail.com>
2023-10-07 17:47:21 +0700

    chore(mock-server): fake file-server to upload file

--- src/components/home/ChooseFileBox.tsx
+++ src/components/home/ChooseFileBox.tsx
@@ -4,9 +4,6 @@ import { ArrowUpTrayIcon } from '@heroicons/react/24/outline';
 import { useOrderWorkflowBox } from '@components/order';
 import { useOrderWorkflowStore } from '@states/order';
 
-// Loc's task in here.
-// This hook is used in ./src/pages/HomePage.tsx.
-
 export function useChooseFileBox() {
   const { openOrderWorkflowBox, OrderWorkflowBox } = useOrderWorkflowBox();
   const [openBox, setOpenBox] = useState<boolean>(false);
@@ -17,44 +14,44 @@ export function useChooseFileBox() {
 
     return (
       <>
-        <div className='relative'>
-          <Dialog open={openBox} handler={handleOpenBox}>
-            <DialogBody
-              className='flex flex-col items-center justify-center gap-5 p-5 lg:p-10'
-              onClick={() => {
-                setOpenBox(false);
-                openOrderWorkflowBox();
-                setOrderStep(1);
-              }}
+        <Dialog size='xs' open={openBox} handler={handleOpenBox}>
+          <DialogBody>
+            <label
+              htmlFor='dropzone-file'
+              className='w-full flex flex-col gap-3 p-4 lg:p-8 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
             >
-              <label
-                htmlFor='dropzone-file'
-                className='flex flex-col w-70 h-35 gap-3 p-4 lg:w-96 lg:h-47 lg:p-8 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
-              >
-                <div className='flex gap-4'>
-                  <ArrowUpTrayIcon
-                    strokeWidth={2}
-                    className='bg-blue/1 text-white rounded-full w-10 h-10 p-2 lg:w-12 lg:h-12 lg:p-3'
-                  />
-                  <span className='justify-center text-xl lg:text-2xl text-blue/1 font-semibold h=10 p-1 lg:h-12 lg:p-2'>
-                    Choose file to print
-                  </span>
-                </div>
+              <div className='flex items-center gap-4'>
+                <ArrowUpTrayIcon
+                  strokeWidth={2}
+                  className='bg-blue/1 text-white rounded-full w-10 h-10 p-2 lg:w-12 lg:h-12 lg:p-3'
+                />
+                <span className='text-xl lg:text-2xl text-blue/1 font-semibold h-10 p-1 lg:h-12 lg:p-2'>
+                  Choose file to print
+                </span>
+              </div>
 
-                <div className='text-sm lg:text-md w-54 h-13 gap-1'>
-                  <span className='font-semibold h-8'>Allowed formats:</span> .doc, .docx, .xls,
-                  .xlsx, .ppt, .jpg, .png, .pdf
-                  <div className='text-sm lg:text-md h-4'>
-                    <span className='font-semibold'>Maximum size:</span> 100MB
-                  </div>
+              <div className='text-sm lg:text-base'>
+                <span className='font-semibold h-8'>Allowed formats:</span> .doc, .docx, .xls,
+                .xlsx, .ppt, .jpg, .png, .pdf
+                <div className='text-sm lg:text-base'>
+                  <span className='font-semibold'>Maximum size:</span> 100MB
                 </div>
+              </div>
 
-                <input id='dropzone-file' type='file' className='hidden' />
-              </label>
-            </DialogBody>
-          </Dialog>
-          {<OrderWorkflowBox />}
-        </div>
+              <input
+                id='dropzone-file'
+                type='file'
+                className='hidden'
+                onChange={() => {
+                  setOpenBox(false);
+                  openOrderWorkflowBox();
+                  setOrderStep(1);
+                }}
+              />
+            </label>
+          </DialogBody>
+        </Dialog>
+        {<OrderWorkflowBox />}
       </>
     );
   };
