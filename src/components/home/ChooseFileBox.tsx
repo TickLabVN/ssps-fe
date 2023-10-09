acdd30ea9b6a7ae24fefb274b198a43ebcb0799b
Nguyễn Minh Lộc <loc.nguyenminh1605@hcmut.edu.vn>
2023-10-09 23:05:54 +0700

    fix(format_tailwind): fix width heigth format

--- src/components/home/ChooseFileBox.tsx
+++ src/components/home/ChooseFileBox.tsx
@@ -23,22 +23,22 @@ export function useChooseFileBox() {
           <DialogBody>
             <label
               htmlFor='dropzone-file'
-              className='flex flex-col items-center  h-35 gap-4 p-4 lg:w-96 lg:h-47 lg:p-7 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
+              className='flex flex-col items-center w-[310px] h-[148px] gap-4 p-4 lg:w-[384px] lg:h-[190px] lg:p-7 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
             >
               <div className='flex gap-4'>
                 <ArrowUpTrayIcon
                   strokeWidth={2}
-                  className='bg-blue/1 text-white rounded-full w-10 h-10 p-2 lg:w-12 lg:h-12 lg:p-3'
+                  className='bg-blue/1 text-white rounded-full w-[40px] h-[40px] p-2 lg:w-[48px] lg:h-[48px] lg:p-3'
                 />
-                <span className='text-xl lg:text-2xl text-blue/1 font-semibold h-10 p-1 lg:h-12 lg:p-2'>
+                <span className='text-xl lg:text-2xl text-blue/1 font-semibold h-[40px] p-1 lg:h-[48px] lg:p-2'>
                   Choose file to print
                 </span>
               </div>
 
-              <div className='text-sm lg:text-base w-54 h-13 gap-1'>
-                <span className='font-semibold h-8'>Allowed formats:</span> .doc, .docx, .xls,
+              <div className='text-sm lg:text-base gap-1'>
+                <span className='font-semibold h-[32px]'>Allowed formats:</span> .doc, .docx, .xls,
                 .xlsx, .ppt, .jpg, .png, .pdf
-                <div className='text-sm lg:text-base h-4'>
+                <div className='text-sm lg:text-base'>
                   <span className='font-semibold'>Maximum size:</span> 100MB
                 </div>
               </div>
