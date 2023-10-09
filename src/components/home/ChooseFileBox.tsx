34754ba4f143408a0f44e8132e2d77dfbe5457e1
Nguyễn Minh Lộc <loc.nguyenminh1605@hcmut.edu.vn>
2023-10-09 21:54:50 +0700

    fix(mobile_box): fix choose file mobile

--- src/components/home/ChooseFileBox.tsx
+++ src/components/home/ChooseFileBox.tsx
@@ -14,13 +14,18 @@ export function useChooseFileBox() {
 
     return (
       <>
-        <Dialog size='xs' open={openBox} handler={handleOpenBox}>
+        <Dialog
+          size='xs'
+          className='2xl:max-w-fit 2xl:w-fit 2xl:min-w-fit lg:max-w-fit lg:w-fit lg:min-w-fit max-w-fit w-fit min-w-fit'
+          open={openBox}
+          handler={handleOpenBox}
+        >
           <DialogBody>
             <label
               htmlFor='dropzone-file'
-              className='w-full flex flex-col gap-3 p-4 lg:p-8 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
+              className='flex flex-col items-center  h-35 gap-4 p-4 lg:w-96 lg:h-47 lg:p-7 border-2 border-blue/1 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-blue-100'
             >
-              <div className='flex items-center gap-4'>
+              <div className='flex gap-4'>
                 <ArrowUpTrayIcon
                   strokeWidth={2}
                   className='bg-blue/1 text-white rounded-full w-10 h-10 p-2 lg:w-12 lg:h-12 lg:p-3'
@@ -30,10 +35,10 @@ export function useChooseFileBox() {
                 </span>
               </div>
 
-              <div className='text-sm lg:text-base'>
+              <div className='text-sm lg:text-base w-54 h-13 gap-1'>
                 <span className='font-semibold h-8'>Allowed formats:</span> .doc, .docx, .xls,
                 .xlsx, .ppt, .jpg, .png, .pdf
-                <div className='text-sm lg:text-base'>
+                <div className='text-sm lg:text-base h-4'>
                   <span className='font-semibold'>Maximum size:</span> 100MB
                 </div>
               </div>
