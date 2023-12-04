d7f1e10aa8fbc2a8987e47122f94564dbbcfdd84
Tan Nguyen <duytan17112003@gmail.com>
2023-12-04 21:59:13 +0700

    feat(logout): add logout settting

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { apiClient, invoke } from './common';
 
 export const authService = {
-  login: (loginData: LoginFormData) => invoke(apiClient.POST('/auth/login', { body: loginData }))
+  login: (loginData: LoginFormData) => invoke(apiClient.POST('/auth/login', { body: loginData })),
+  logout: () => invoke(apiClient.DELETE('/auth/logout'))
 };
